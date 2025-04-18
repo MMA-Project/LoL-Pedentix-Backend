d827729826f5b37490eb2a8541a088f9e4270e05
Alexandre Clenet <alexandreclenet@free.fr>
2025-04-18 15:31:56 +0200

    no pupperteer store champions data

--- src/routes/game.ts
+++ src/routes/game.ts
@@ -5,16 +5,29 @@ import { getMaskedText } from "../utils/words";
 import path from "path";
 import fs from "fs";
 import { getDailySeed } from "../utils/seed";
-import { getLeaguePedantixfromSeed } from "../models/LeaguePedantix";
+import {
+  champions,
+  LeaguePedantix,
+  LeaguePedantixModel,
+} from "../models/LeaguePedantix";
 import cron from "node-cron";
+import puppeteer from "puppeteer";
 
 const router = express.Router();
 
 const GAMES_DIR = path.join(__dirname, "..", "data", "games");
+const CHAMP_DIR = path.join(__dirname, "..", "data", "champions");
 
 if (!fs.existsSync(GAMES_DIR)) {
   fs.mkdirSync(GAMES_DIR, { recursive: true });
 }
+
+if (!fs.existsSync(CHAMP_DIR)) {
+  fs.mkdirSync(CHAMP_DIR, { recursive: true });
+}
+
+const getChampionFilePath = (name: string) =>
+  path.join(CHAMP_DIR, `${name}.json`);
 
 const getGameFilePath = (id: string) => path.join(GAMES_DIR, `${id}.json`);
 
@@ -26,8 +39,23 @@ const saveGameToFile = (game: Game) => {
   );
 };
 
+const saveChampionToFile = (LeaguePedantix: LeaguePedantixModel) => {
+  fs.writeFileSync(
+    getChampionFilePath(LeaguePedantix.name),
+    JSON.stringify(LeaguePedantix, null, 2),
+    "utf-8"
+  );
+};
+
 const loadGameFromFile = (id: string): Game | null => {
   const filePath = getGameFilePath(id);
+  if (!fs.existsSync(filePath)) return null;
+  const content = fs.readFileSync(filePath, "utf-8");
+  return JSON.parse(content);
+};
+
+const loadChampionFromFile = (name: string): LeaguePedantixModel | null => {
+  const filePath = getChampionFilePath(name);
   if (!fs.existsSync(filePath)) return null;
   const content = fs.readFileSync(filePath, "utf-8");
   return JSON.parse(content);
@@ -153,3 +181,46 @@ cron.schedule("0 0 * * *", () => {
   console.log("Old games cleared at midnight.");
 });
 clearOldGames();
+
+export const getAllLeaguePedantix = async (): Promise<void> => {
+  for (let i = 0; i < champions.length; i++) {
+    const champion = champions[i];
+    const url = `https://universe.leagueoflegends.com/fr_FR/story/champion/${champion}/`;
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto(url, {
+      waitUntil: "networkidle0",
+    });
+
+    const text = await page.$$eval(".p_1_sJ", (elements) =>
+      elements.map((el) => el.textContent?.trim()).join(" \n")
+    );
+
+    const image = await page.$eval(".image_3oOd", (el) => {
+      const style = el.getAttribute("style") || "";
+      const match = style.match(/url\(['"]?(.*?)['"]?\)/);
+      return match ? match[1] : "";
+    });
+
+    console.log(champion, image);
+    saveChampionToFile(new LeaguePedantix(champion, image, text));
+
+    await browser.close();
+  }
+};
+//getAllLeaguePedantix();
+
+export const getLeaguePedantixfromSeed = async (
+  seed: number
+): Promise<LeaguePedantixModel> => {
+  const seededRandom = (seed: number): number => {
+    const x = Math.sin(seed) * 10000;
+    return x - Math.floor(x);
+  };
+
+  const name = champions[Math.floor(seededRandom(seed) * champions.length)];
+  const champion = loadChampionFromFile(name);
+  return champion!!;
+};
